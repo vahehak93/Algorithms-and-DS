1bea8a092a304c3996df98472e55026218a3ede2
vahehak93 <vahehak@gmail.com>
2022-02-21 05:40:05 +0400

    feat: final task

--- recursion_1.js
+++ recursion_1.js
@@ -1,4 +1,4 @@
-// Write a recursive function to determine whether all digits of the number are odd or not.
+/* Write a recursive function to determine whether all digits of the number are odd or not.*/
 
 function recursiveOdd(num) {
   let oddCounter = 0;
