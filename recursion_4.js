5318b607fc3331aa5542e318af7b07f294c3320f
vahehak93 <vahehak@gmail.com>
2022-02-21 05:40:22 +0400

    feat: final task

--- recursion_4.js
+++ recursion_4.js
@@ -1,5 +1,5 @@
 /*Given an array. Write a recursive function that removes the first element and returns the
-given array. (without using arr.shift()) */
+given array. (without using arr.shift())         */
 
 function recursiveShift(arr, count = 0) {
   if (count === 0) {
